f46cfd350ac3eec3bd76e424e37989b06e32dcbd
Saize <saize2saize@gmail.com>
2023-02-04 01:47:10 +0530

    First commit

---
+++ file handling.py
@@ -0,0 +1,13 @@
+# f=open('sample.txt','r')
+# data=f.read()
+# print(data)
+# f.close()
+
+# write in a file
+
+# f=open('another.txt','a')
+# f.write("Mohammad Saize Ali")
+# f.close()
+with open("another.txt",'r')as f:
+   a= f.read()
+print(a)
